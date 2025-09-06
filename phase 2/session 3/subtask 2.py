6aad334051c3e282f5c4b999ca058ee524489dca
janasabri26 <janasabri26@gmail.com>
2025-09-07 02:20:12 +0300

    Update subtask 2.py

--- phase 2/session 3/subtask 2.py
+++ phase 2/session 3/subtask 2.py
@@ -4,39 +4,29 @@ import matplotlib.pyplot as plt
 
 img=cv2.imread("shapes.jpg")
 out=img.copy()
-#bgr
-blue_lower=np.array([200,0,0])
-blue_upper=np.array([255,100,100])
 
-red_lower=np.array([0,0,200])
-red_upper=np.array([100,100,255])
+blue=[255,0,0]
+red=[0,0,255]
+black=[0,0,0]
 
-black_lower=np.array([0,0,0])
-black_upper=np.array([50,50,50])
+blue_mask=(img[:,:,0]>150)&(img[:,:,1]<80)&(img[:,:,2]<80)
+red_mask=(img[:,:,0]<80)&(img[:,:,1]<80)&(img[:,:,2]>150)
+black_mask=(img[:,:,0]<50)&(img[:,:,1]<50)&(img[:,:,2]<50)
 
-
-
-blue_mask=cv2.inRange(img,blue_lower,blue_upper)
-red_mask=cv2.inRange(img,red_lower,red_upper)
-black_mask=cv2.inRange(img,black_lower,black_upper)
-
-
-out[blue_mask>0]=[0,0,0]
-out[red_mask>0]=[255,0,0]
-out[black_mask>0]=[0,0,255]
-
-
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-out_rgb = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
-
+out[blue_mask]=[0,0,0]
+out[red_mask]=[255,0,0]
+out[black_mask]=[0,0,255]
 
 
 
 fig, axes = plt.subplots(1, 2)
-axes[0].imshow(img_rgb)
+
+axes[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 axes[0].set_title('Original Image')
 axes[0].axis('off')
-axes[1].imshow(out_rgb)
+axes[1].imshow(cv2.cvtColor(out, cv2.COLOR_BGR2RGB))
 axes[1].set_title('Processed Image')
 axes[1].axis('off')
+plt.show()
+
 plt.show()
